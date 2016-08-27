2b96a1eb23f7172d93530fb8263fc23bb70f7c74
Fion <fiondev7@gmail.com>
2016-08-27 17:04:48 +0800

    刪除pipe Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ArticleComponent } from './article/article.component';
-import { PipeComponent } from './pipe/pipe.component';
+
 import { HighlightPipe } from './highlight.pipe';
 
 import { HeroService } from './hero.service';
@@ -13,7 +13,7 @@ import { HeroService } from './hero.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent, ArticleComponent, PipeComponent, HighlightPipe
+    HeaderComponent, ArticleComponent, HighlightPipe
   ],
   imports: [
     BrowserModule,
